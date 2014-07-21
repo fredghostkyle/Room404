e1563a89868696a321510187c126476b80c066ff
fredghostkyle <fredghostkyle@yahoo.com>
2014-07-20 23:43:57 -0400

    removed a comment

--- index.php
+++ index.php
@@ -4,10 +4,6 @@
 //$text = file_get_contents($textPath, FILE_USE_INCLUDE_PATH);
 //$textPath = "./comicfiles/"+$page+"/lines.txt",
 
-//spoiler
-//$spoiler = file_get_contents($spoilerPath, FILE_USE_INCLUDE_PATH);
-//$spoilerPath = "./comicfiles/"+$page+"/spoiler.txt",
-
 //image
 // $image = "./comicfiles/"+$page+"/image.png";
 
@@ -51,6 +47,7 @@ else {
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
   </head>
 
   <body>
