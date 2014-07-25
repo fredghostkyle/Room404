3031e44d82bb6ec2e8508311e34e9fdfe4eb10e5
fredghostkyle <fredghostkyle@yahoo.com>
2014-07-24 22:47:20 -0400

    moved my TODO list

--- index.php
+++ index.php
@@ -2,14 +2,6 @@
 //Remove errors from the page.
 error_reporting(0); 
 
-/* TODO LIST: 
- * Add 13+ warning expires after ~30d
- * get domains -- room404comic.com; room404comic.org; rm404.in
- * get the Cog/Gear working across the whole website. 
- * Working on $welcome popup
- * Create Welcome compic picture
- */
-
 
 /*
  * Setting up the comic's settings
---
+++ todo.md
@@ -0,0 +1,9 @@
+To-Do List
+=======
+
+[ ] Add 13+ warning expires after ~30d
+[ ] get domains -- room404comic.com; room404comic.org; rm404.in
+[ ] get the Cog/Gear working across the whole website. 
+[ ] Working on $welcome popup
+[ ] Create Welcome compic picture
+[x] Update website's name~~
