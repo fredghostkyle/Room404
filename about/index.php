839b3e2ffe26ee56440fc1d4bdbe70a0adc86f89
fredghostkyle <fredghostkyle@yahoo.com>
2014-07-24 21:25:58 -0400

    added abouts' for Lauren & Clay

--- about/index.php
+++ about/index.php
@@ -84,7 +84,7 @@
       <div class="row">
        <div class="col-6 col-sm-6 col-lg-4">
               <h2>Lauren</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p>Lauren is a writer and artist of primarly fan work, but offten works on original works of art. She came up with the idea for a webcomic and controls the beast that it is. Her job is to mainly create the plot and orginise the project then giving the final okay. </p>
               <p><a class="btn btn-default" href="./" role="button">View details »</a></p>
             </div><!--/span-->
             <div class="col-6 col-sm-6 col-lg-4">
@@ -109,7 +109,7 @@
             </div><!--/span-->
             <div class="col-6 col-sm-6 col-lg-4">
               <h2>Clay</h2>
-              <p>I enjoy long walks on the beach. I enjoy long walks on the beach. I enjoy long walks on the beach.</p>
+              <p>Clay is a flambooint young man who loves theater. He has a deep admeratoin for Spider Mand(tm). He also likes to dress very nicly. He comes up with 30 jokes to make people lafe. I worked on the plot and charetors and I'm helping with the music. (I am also sleeping with you wife &lt;3)</p>
               <p><a class="btn btn-default" href="http://fb.com/" role="button">FB</a></p>
             </div><!--/span-->
           </div>
